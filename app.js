4528e8ac220733d6edb35cc043581800bcd36dcc
kdpark <kdpark@kw.ac.kr>
2022-10-23 01:36:06 +0900

    dotenv 처리

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+require("dotenv").config({ path: "./.env" });
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var customersRouter = require("./routes/api/customers");
--- mysql/index.js
+++ mysql/index.js
@@ -2,12 +2,12 @@ const mysql = require("mysql");
 const sql = require("./sql.js");
 
 const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: "127.0.0.1",
-  port: 3306,
-  user: "dev01",
-  password: "1234",
-  database: "kwic",
+  connectionLimit: process.env.MYSQL_LIMIT,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
 });
 
 const query = async (alias, values) => {
