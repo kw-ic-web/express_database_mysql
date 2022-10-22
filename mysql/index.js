41bec36746667350ad075aa331076df502ddbe0e
kdpark <kdpark@kw.ac.kr>
2022-10-23 01:30:26 +0900

    조회 기능

---
+++ mysql/index.js
@@ -0,0 +1,28 @@
+const mysql = require("mysql");
+const sql = require("./sql.js");
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "127.0.0.1",
+  port: 3306,
+  user: "dev01",
+  password: "1234",
+  database: "kwic",
+});
+
+const query = async (alias, values) => {
+  return new Promise((resolve, reject) =>
+    pool.query(sql[alias], values, (error, results) => {
+      if (error) {
+        console.log(error);
+        reject({
+          error,
+        });
+      } else resolve(results);
+    })
+  );
+};
+
+module.exports = {
+  query,
+};
